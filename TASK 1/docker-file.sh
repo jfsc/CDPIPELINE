afb9d2d1c24dafd56e7e9abef31ff789adf31600
docker <docker@vm.dir.svc.accenture.com>
2019-12-23 11:58:50 -0300

    correction time db

--- TASK 1/docker-file.sh
+++ TASK 1/docker-file.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 
-time \
-(docker network create springlab \
-&& \
+time (echo "=> Create application network"
+docker network create springlab \
+
 docker build --build-arg url=https://github.com/romuloslv/spring-petclinic.git \
              --build-arg project=spring-petclinic \
              --build-arg artifactid=spring-petclinic \
              --build-arg version=2.2.0 \
              -t img-petclinic - < Dockerfile \
-&& \
+
+echo "=> Build database image"
 docker run --name mysql \
            --network springlab \
            -e MYSQL_ROOT_PASSWORD=petclinic \
            -e MYSQL_DATABASE=petclinic \
            -p 3306:3306 \
            -d mysql:5.7.8 \
-&& \
+
+sleep 10
+echo "=> Build application image"
 docker run --name petclinic \
            --network springlab \
            -p 8080:8080 \
-           -d img-petclinic \
+           img-petclinic \
 
 echo "=> Removing <none> images"
 docker rmi $(docker images -f "dangling=true" -q)
