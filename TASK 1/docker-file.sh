b968faf029da2d343e5b420c98fd22e4f8a9e594
docker <docker@vm.dir.svc.accenture.com>
2019-12-24 09:51:44 -0300

    Container start repair

--- TASK 1/docker-file.sh
+++ TASK 1/docker-file.sh
@@ -17,9 +17,6 @@ docker run --name mysql \
            -p 3306:3306 \
            -d mysql:5.7.8 \
 
-echo "=> Database turn off"
-sleep 10
-echo "=> Database turn on"
 echo "=> Build application image"
 docker run --name petclinic \
            --network springlab \
