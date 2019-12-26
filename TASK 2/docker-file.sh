2f601e50e2c34ddccd5146151e04c45f3a802476
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2019-12-26 02:05:47 -0300

    Update docker-file.sh

--- TASK 2/docker-file.sh
+++ TASK 2/docker-file.sh
@@ -16,39 +16,39 @@ docker volume create sonarqube_logs
 docker volume create sonarqube_data
 
 echo "=> Building database image"
-docker run -d --name postgres \\
-           --network task2 \\
-           -e POSTGRES_USER=sonarqube \\
-           -e POSTGRES_PASSWORD=sonarqube \\
-           -e POSTGRES_DB=sonarqube \\
-           -p 5432:5432 \\
-           -v postgres_data:/var/lib/postgresql/data \\
+docker run -d --name postgres \
+           --network task2 \
+           -e POSTGRES_USER=sonarqube \
+           -e POSTGRES_PASSWORD=sonarqube \
+           -e POSTGRES_DB=sonarqube \
+           -p 5432:5432 \
+           -v postgres_data:/var/lib/postgresql/data \
            postgres
 
-docker build -t romuloslv/jenkins \\
+docker build -t romuloslv/jenkins \
              -f Docker-Jenkins .
 
 echo "=> Building jenkins application"
-docker run -d --name jenkins \\
-           --network task2 \\
-           -p 9090:8080 \\
-           -p 5000:5000 \\
-           -v jenkins_data:/var/jenkins_home \\
-           -v /var/run/docker.sock:/var/run/docker.sock \\
-	       romuloslv/jenkins
+docker run -d --name jenkins \
+           --network task2 \
+           -p 9090:8080 \
+           -p 5000:5000 \
+           -v jenkins_data:/var/jenkins_home \
+           -v /var/run/docker.sock:/var/run/docker.sock \
+	   romuloslv/jenkins
 
-docker build -t romuloslv/sonarqube \\
+docker build -t romuloslv/sonarqube \
              -f Docker-Sonarqube .
 
 echo "=> Building sonarqube application"
-docker run -d --name sonarqube \\
-           --network task2 \\
-           -p 9000:9000 \\
-           -e SONARQUBE_JDBC_URL=jdbc:postgresql://postgres:5432/sonarqube \\
-           -e SONARQUBE_JDBC_USERNAME=sonarqube \\
-           -e SONARQUBE_JDBC_PASSWORD=sonarqube \\
-           -v sonarqube_conf:/opt/sonarqube/conf \\
-           -v sonarqube_extensions:/opt/sonarqube/extensions \\
-           -v sonarqube_logs:/opt/sonarqube/logs \\
-           -v sonarqube_data:/opt/sonarqube/data \\
+docker run -d --name sonarqube \
+           --network task2 \
+           -p 9000:9000 \
+           -e SONARQUBE_JDBC_URL=jdbc:postgresql://postgres:5432/sonarqube \
+           -e SONARQUBE_JDBC_USERNAME=sonarqube \
+           -e SONARQUBE_JDBC_PASSWORD=sonarqube \
+           -v sonarqube_conf:/opt/sonarqube/conf \
+           -v sonarqube_extensions:/opt/sonarqube/extensions \
+           -v sonarqube_logs:/opt/sonarqube/logs \
+           -v sonarqube_data:/opt/sonarqube/data \
            romuloslv/sonarqube)
